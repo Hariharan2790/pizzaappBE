4d1366deb6b8473094651185443d6f0544e4b470
hariharan <hariharan.mct@gmail.com>
2022-04-05 10:08:37 +0530

    commit changes

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 require('colors')
+require("dotenv").config();
 
 const connectDB = async ()=> {
     try {
-        const url = process.env.MONGODB_URI;
+        const url = process.env.MONGO_URL;
         const conn = await mongoose.connect(url, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
